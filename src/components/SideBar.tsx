9175d6f8ac4731b665f47f75708261caccbb03a8
AgnaldoCordeiro <agske13@gmail.com>
2021-12-03 10:49:08 -0300

    adicionando Content, sidebar e hoooks e types

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
+import { Button } from '../components/Button';
+import '../styles/sidebar.scss';
+import useIndex from '../hooks/useIndex';
+
 export function SideBar() {
-  // Complete aqui
+  const {   
+    selectedGenreId,
+    genres,
+    handleClickButton
+  } = useIndex();
+ 
+  return (
+     <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}              
+              selected={selectedGenreId === genre.id}
+            />          
+          ))}
+        </div>
+
+      </nav>
+  )
 }
---
+++ src/hooks/useIndex.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { GenreResponseProps } from '../@types/GenreResponse';
+import { MovieProps } from '../@types/Movie';
+import { api } from '../services/api';
+
+export default function useIndex() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1),
+    [genres, setGenres] = useState<GenreResponseProps[]>([]),
+    [movies, setMovies] = useState<MovieProps[]>([]),
+    [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return {
+    selectedGenreId,
+    genres,
+    movies,
+    selectedGenre,
+    handleClickButton,
+  };
+  
+}
